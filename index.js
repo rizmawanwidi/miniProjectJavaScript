b69ba8739ca304b37ae80bc72028a7792598b5cc
Rizmawan Widi Wiranata <rizmawanwidi@gmail.com>
2024-02-24 20:37:23 +0700

    v4 JS

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const displayHistory = document.querySelector(".display-history");
 const display = document.querySelector(".display-input");
 const tempResult = document.querySelector(".temp-result");
 const numbers = document.querySelectorAll(".number");
-const operation = document.querySelectorAll(".operation");
+const operations = document.querySelectorAll(".operation");
 const equal = document.querySelector(".equal");
 const clearAll = document.querySelector(".all-clear");
 const clearLast = document.querySelector(".last-entity-clear")
 
 let dis1Num = "";
 let dis2Num = "";
-let result = null;
+let results = null;
 let lastOperation = "";
 let haveDot = false;
 
@@ -22,6 +22,44 @@ numbers.forEach ((number) => {
             console.log('sudah ada dot',e.target.innerText)
             return;
         }
+        dis2Num += e.target.innerText;
+        display.innerText = dis2Num;
         
     })
+});
+
+operations.forEach((operation) => {
+    operation.addEventListener("click", (e) =>{
+        if (!dis2Num) return;
+        haveDot = false;
+        const operationName = e.target.innerText;
+        if (dis1Num && dis2Num && lastOperation) {
+            console.log("menjalankan operasi matematika")
+            mathOperation()
+        } else {
+            result = parseFloat (dis2Num);
+        }
+        clearVar(operationName)
+        lastOperation = operationName
+    })
 })
+function clearVar(name = "") {
+    dis1Num += dis2Num + " " + name + " ";
+    displayHistory.innerText = dis1Num;
+    display.innerText = "";
+    dis2Num = ""
+    tempResult.innerText = result;
+}
+function mathOperation () {
+    if (lastOperation === "X") {
+        result = parseFloat(result) * parseFloat(dis2Num)
+    } else if (lastOperation === "+") {
+        result = parseFloat(result) + parseFloat(dis2Num)
+    } else if (lastOperation === "-") {
+        result = parseFloat(result) - parseFloat(dis2Num)
+    } else if (lastOperation === "/") {
+        result = parseFloat(result) / parseFloat(dis2Num)
+    } else if (lastOperation === "%") {
+        result = parseFloat(result) % parseFloat(dis2Num)
+    }
+}
