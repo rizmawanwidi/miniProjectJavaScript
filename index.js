688e3affd83c4ee24b72882b53d2cc285830ab8a
Rizmawan Widi Wiranata <rizmawanwidi@gmail.com>
2024-02-24 20:00:17 +0700

    v3 JS

---
+++ index.js
@@ -0,0 +1,27 @@
+const displayHistory = document.querySelector(".display-history");
+const display = document.querySelector(".display-input");
+const tempResult = document.querySelector(".temp-result");
+const numbers = document.querySelectorAll(".number");
+const operation = document.querySelectorAll(".operation");
+const equal = document.querySelector(".equal");
+const clearAll = document.querySelector(".all-clear");
+const clearLast = document.querySelector(".last-entity-clear")
+
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
+
+numbers.forEach ((number) => {
+    number.addEventListener("click", (e) => {
+        if (e.target.innerText === "." && !haveDot) {
+            console.log(e.target.innerText)
+            haveDot = true
+        } else if (e.target.innerText === "." && haveDot) {
+            console.log(e.target.innerText)
+            return;
+        }
+        
+    })
+})
